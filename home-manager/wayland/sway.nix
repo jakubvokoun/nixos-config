7f9194674f2fddac8287f7b3015fc8591f3d15d6
Jakub Vokoun <jakub.vokoun@gmail.com>
2025-04-23 18:20:19 +0200

    chore(sway): clean up & wlgout update

--- home-manager/wayland/sway.nix
+++ home-manager/wayland/sway.nix
@@ -56,7 +56,47 @@
     };
   };
 
-  programs.wlogout = { enable = true; };
+  programs.wlogout = {
+    enable = true;
+    layout = [
+      {
+        label = "lock";
+        action = "sh -c '(sleep 0.5s; ${pkgs.swaylock}/bin/swaylock)'";
+        text = "Lock";
+        keybind = "l";
+      }
+      {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "Hibernate";
+        keybind = "h";
+      }
+      {
+        label = "logout";
+        action = "loginctl terminate-user $USER";
+        text = "Logout";
+        keybind = "e";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+      }
+      {
+        label = "suspend";
+        action = "systemctl suspend";
+        text = "Suspend";
+        keybind = "u";
+      }
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "r";
+      }
+    ];
+  };
 
   programs.rofi = {
     enable = true;
